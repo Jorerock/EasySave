8470bc66556ee516b7ee472c8440ae8fc067f6e2
Jorerock <jorerock@laposte.net>
2026-02-03 14:13:47 +0100

    Ajoute la classe Backupjob et l'énumération BackupType
    Création du fichier `Backupjob.cs` avec la classe interne `Backupjob` pour représenter une tâche de sauvegarde, incluant ses propriétés principales. Ajout de l'énumération `BackupType` pour distinguer les types de sauvegarde (complète ou différentielle). Prépare la base pour la gestion des tâches de sauvegarde.

---
+++ Backupjob.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Backupjob
+{
+    internal class Backupjob
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string SourceDirectory { get; set; } = string.Empty;
+        public string TargetDirectory { get; set; } = string.Empty;
+        public BackupType Type { get; set; }
+        public string LastBackupDate { get; set; } = string.Empty;
+    }
+  
+}
+
+
+public enum BackupType
+{
+    Full,
+    Differential
+}
