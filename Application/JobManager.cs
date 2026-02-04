e92261ed0f5d077cd3579ee56d6a277cffc0e9fa
jasonivan <jasonivankouam@gmail.com>
2026-02-04 16:46:21 +0100

    Creation de la classe JobManager

---
+++ Application/JobManager.cs
@@ -0,0 +1,75 @@
+﻿using EasySave.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace EasySave.Application
+{
+    internal class JobManager
+    {
+        private  IJobRepository _repo;
+        private  int MaxJobs = 5;
+
+        public JobManager(IJobRepository repo)
+        {
+            if (repo == null)
+            {
+                throw new ArgumentNullException(nameof(repo));
+            }
+
+            _repo = repo;
+        }
+
+        public List<BackupJob> GetAll()
+        {
+            return _repo.LoadAll();
+        }
+
+        public void Add(BackupJob job)
+        {
+            if (job == null)
+            {
+                throw new ArgumentNullException(nameof(job));
+            }
+
+            List<BackupJob> jobs = _repo.LoadAll();
+
+            if (jobs.Count >= MaxJobs)
+            {
+                throw new InvalidOperationException(
+                    "Nombre maximum de travaux  atteint."
+                );
+            }
+
+            bool idAlreadyExists = jobs.Any(existingJob => existingJob.Id == job.Id);
+            if (idAlreadyExists)
+            {
+                throw new InvalidOperationException(
+                    " existe déjà."
+                );
+            }
+
+            jobs.Add(job);
+            _repo.SaveAll(jobs);
+        }
+
+        public void Remove(int id)
+        {
+            List<BackupJob> jobs = _repo.LoadAll();
+
+            BackupJob? jobToRemove =
+                jobs.FirstOrDefault(existingJob => existingJob.Id == id);
+
+            if (jobToRemove == null)
+            {
+                throw new InvalidOperationException(
+                    "Le travail à supprimer est introuvable."
+                );
+            }
+
+            jobs.Remove(jobToRemove);
+            _repo.SaveAll(jobs);
+        }
+    }
+}
