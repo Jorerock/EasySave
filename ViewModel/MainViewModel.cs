bd6efccccd85c2596527575ad1b33761aa2fa9f7
Djoumessi <108170953+velosso@users.noreply.github.com>
2026-02-04 16:21:30 +0100

    feat: ajout du MainViewModel pour l’orchestration des actions de la vue
    - Implémentation du MainViewModel
    - Exposition des opérations de gestion des sauvegardes
    - Délégation des actions au JobManager et au BackupOrchestrator
    - Respect de l’architecture MVVM (aucun accès direct aux fichiers)

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,53 @@
+﻿using EasySave.Application;
+using EasySave.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasySave.ViewModel
+{
+    public sealed class MainViewModel
+    {
+        private readonly JobManager _jobs;
+        private readonly BackupOrchestrator _orchestrator;
+
+        public MainViewModel(JobManager jobs, BackupOrchestrator orchestrator)
+        {
+            _jobs = jobs;
+            _orchestrator = orchestrator;
+        }
+
+        public List<BackupJob> ListJobs()
+        {
+            return _jobs.GetAll();
+        }
+
+        public void CreateJob(string name, string src, string dst, BackupType type)
+        {
+            BackupJob job = new BackupJob
+            {
+                Name = name,
+                SourceDirectory = src,
+                TargetDirectory = dst,
+                Type = type
+            };
+
+            _jobs.Add(job);
+        }
+
+        public void DeleteJob(int id)
+        {
+            _jobs.Remove(id);
+        }
+
+        public void RunJobs(List<int> ids)
+        {
+            _orchestrator.RunMany(ids);
+        }
+
+        public void RunAll()
+        {
+            _orchestrator.RunAllSequential();
+        }
+    }
+}
