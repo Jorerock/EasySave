8300884c6c5c35f66bf6119c4d8321a65f42208b
Jorerock <jorerock@laposte.net>
2026-02-14 15:33:31 +0100

    Intégration de CryptoSoft pour le chiffrement des fichiers
    Ajout de la référence à la DLL CryptoSoft dans le projet et inclusion du binaire. Création de la classe CryptoSoftEncryptorAdaptater pour encapsuler l'utilisation de CryptoSoft et permettre le chiffrement de fichiers via un adaptateur dédié.

---
+++ Infrastructure/CryptoSoftEncryptorAdaptater.cs
@@ -0,0 +1,22 @@
+﻿using CryptoSoft;
+
+namespace EasySave.Infrastructure
+{
+    internal class CryptoSoftEncryptorAdaptater
+    {
+        internal required string _CryptoSoftExePath;
+
+        public void CryptoSoftEncryptorAdapter(string CryptoSoftExePath)
+        {
+            _CryptoSoftExePath = CryptoSoftExePath ?? throw new ArgumentNullException(nameof(CryptoSoftExePath));
+        }
+
+        public void EncryptFile(string inputFilePath, string outputFilePath)
+        {
+            string key = "si j'oublie de retirer la cle du code celestin je suis dans la mouise";
+            CryptoSoft.FileManager manager = new FileManager(inputFilePath, key);
+            int elapsed = manager.TransformFile();
+
+        }
+    }
+}
