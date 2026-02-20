b2c26b521ea8681494700af9f71752160bea9650
jasonivan <jasonivankouam@gmail.com>
2026-02-20 10:13:42 +0100

    blocage metiers

---
+++ EasySave.Core/Infrastructure/ProcessBusinessSoftwareDetector.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace EasySave.Core.Infrastructure
+{
+    public class ProcessBusinessSoftwareDetector : IBusinessSoftwareDetector
+    {
+        private readonly string _processPath; // Ou une liste issue des AppSettings
+
+        public bool IsBlocked()
+        {
+            // Logique : vérifier si l'un des processus définis dans les paramètres est actif
+            return Process.GetProcesses().Any(p => p.ProcessName.Contains("LogicielMetier"));
+        }
+    }
+}
